63aab242dcea9e5d8d5e460064234407781e60c1
Brandon Perard <brandon.perard@gmail>
2021-01-12 20:01:03 -0800

    add functionality for one round with pseudocode placeholders in structure

--- app.js
+++ app.js
@@ -12,13 +12,12 @@ const totalLossSpan = document.querySelector('#total-losses');
 const gameControl = document.querySelector('#game-controls');
 const userOutcome = document.querySelector('#user-outcome');
 
-const guessQualify = document.querySelector('#guess-qualifier');
-const endGameMsg = document.querySelector('#end-game-message');
+const userNotification = document.querySelector('#user-notification');
 
 // Initial State
 let totalWins = 0;
 let totalLosses = 0;
-
+let guesses = 4;
 
 startGameButt.addEventListener('click', () => {
     startGameButt.classList.add('hidden');
@@ -27,11 +26,40 @@ startGameButt.addEventListener('click', () => {
 });
 
 submitGuessButt.addEventListener('click', () => {
+    runGame(guesses);
+});
+
+function runGame() {
+    const userGuess = guessInput.valueAsNumber;
+    console.log(guesses);
     const correctNumber = Math.ceil(Math.random() * 20);
-    if (guessInput.valueAsNumber > 0 && guessInput.valueAsNumber <= 20 && guessInput.valueAsNumber % 1 === 0) {
-        console.log(correctNumber);
+    if (userGuess > 0 && userGuess <= 20 && userGuess % 1 === 0) {
+        if (userGuess === correctNumber) {
+            console.log('Winner');
+        } else if (userGuess > correctNumber) {
+            console.log('Too high');
+        } else {
+            console.log('Too low');
+        }
+
+        if ('Winner') {
+            console.log('Winner');
+        } else if ('Too high') {
+            userOutcome.classList.remove('hidden');
+            userNotification.textContent = 'Too high';
+            console.log('Too high');
+        } else {
+            userOutcome.classList.remove('hidden');
+            userNotification.textContent = 'Too low';
+        }
+
+
+        console.log(typeof guesses, guesses);
+
     } else {
         console.log('Please guess an integer 1-20');
     }
-
-})
+    guesses--;
+    console.log(guesses);
+    return guesses;
+}
