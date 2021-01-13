0e21635551cbf70eb6e2421d5070e9c9aea9325b
Brandon Perard <brandon.perard@gmail>
2021-01-12 20:52:35 -0800

    add click handler on each button and separate function for start/reset game

--- app.js
+++ app.js
@@ -17,22 +17,30 @@ const userNotification = document.querySelector('#user-notification');
 // Initial State
 let totalWins = 0;
 let totalLosses = 0;
-let guesses = 4;
+let guesses;
+let correctNumber;
 
 startGameButt.addEventListener('click', () => {
     startGameButt.classList.add('hidden');
     userOutcome.classList.add('hidden');
     gameControl.classList.remove('hidden');
+    startGame();
 });
 
 submitGuessButt.addEventListener('click', () => {
-    runGame(guesses);
+    runGame();
+});
+
+resetGameButt.addEventListener('click', () => {
+    userOutcome.classList.add('hidden');
+    submitGuessButt.classList.remove('hidden');
+    startGame();
 });
 
 function runGame() {
     const userGuess = guessInput.valueAsNumber;
     console.log(guesses);
-    const correctNumber = Math.ceil(Math.random() * 20);
+
     if (userGuess > 0 && userGuess <= 20 && userGuess % 1 === 0) {
         if (userGuess === correctNumber) {
             console.log('Winner');
@@ -42,8 +50,10 @@ function runGame() {
             console.log('Too low');
         }
 
-        if ('Winner') {
-            console.log('Winner');
+        if (false) {
+            // userOutcome.classList.remove('hidden');
+            // userNotification.textContent = 'Winner';
+            // submitGuessButt.classList.add('hidden');
         } else if ('Too high') {
             userOutcome.classList.remove('hidden');
             userNotification.textContent = 'Too high';
@@ -58,8 +68,14 @@ function runGame() {
 
     } else {
         console.log('Please guess an integer 1-20');
+        return;
     }
     guesses--;
     console.log(guesses);
-    return guesses;
+}
+
+function startGame() {
+    guesses = 4;
+    correctNumber = Math.ceil(Math.random() * 20);
+    console.log(correctNumber);
 }
